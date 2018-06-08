23a7190c0bfeca31755b9353d5db2ec710d1df26
App Academy Student <olivermartinball@gmail.com>
2018-06-08 17:25:24 -0400

    end of day commit

--- building active record/lib/02_searchable.rb
+++ building active record/lib/02_searchable.rb
@@ -3,10 +3,29 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_arr = []
+    question_marks = []
+    params.each do |key, value| 
+      where_arr << "#{key} = ?"
+      question_marks << value
+    end
+    where_str = where_arr.join(" AND ")
+    
+    data = DBConnection.instance.execute(<<-SQL, *question_marks)
+    SELECT
+      *
+    FROM 
+      #{self.table_name}
+    WHERE
+      #{where_str}
+    SQL
+    
+    data.map { |datum| self.new(datum) }
+    
   end
 end
 
-class SQLObject
-  # Mixin Searchable here...
+class SQLObject 
+  extend Searchable
+  
 end
--- building active record/lib/03_associatable.rb
+++ building active record/lib/03_associatable.rb
@@ -10,34 +10,76 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.class_name.to_s.downcase + 's'
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (name.to_s + '_id').to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (self_class_name.to_s.downcase + '_id').to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.camelcase.singularize
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      
+      data = DBConnection.instance.execute(<<-SQL)
+        SELECT
+          #{options.table_name}.*
+        FROM
+          #{self.class.table_name}
+        JOIN
+          #{options.table_name} ON 
+            #{self.class.table_name}.#{options.foreign_key} = 
+            #{options.table_name}.#{options.primary_key}
+        WHERE
+          #{self.class.table_name}.id = #{self.id}      
+      SQL
+      
+      return nil if data.empty?
+      options.class_name.constantize.new(data.first)
+
+
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.class.to_s, options)
+    
+    define_method(name) do 
+      data = DBConnection.instance.execute(<<-SQL)
+        SELECT
+          #{options.table_name}.*
+        FROM 
+          #{self.class.table_name}
+        JOIN 
+          #{options.table_name} ON   
+          #{self.class.table_name}.#{options.primary_key} = 
+          #{options.table_name}.#{options.foreign_key}
+        WHERE 
+          #{self.class.table_name}.id = #{self.id}
+      SQL
+      return [] if data.empty?
+      data.map{ |datum| options.class_name.constantize.new(datum)}
+    end
+    
   end
 
   def assoc_options
@@ -46,5 +88,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
